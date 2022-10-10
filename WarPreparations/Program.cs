64fc9d354885cf25a837f035afbef8c8cca3c8ce
iancarpenter <ianhcarpenter@gmail.com>
2022-10-10 22:40:59 +0100

    Add project files.

---
+++ WarPreparations/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace WarPreparations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sword s = new Sword(Material.Bronze, Gemstone.Sapphire, 15, 2);
+
+            Sword orig = new Sword(Material.Iron, Gemstone.None);
+
+            Sword origWithGem = orig with { gemstone = Gemstone.Sapphire };
+
+            Sword origWithAll = orig with { gemstone = Gemstone.Bitstone, crossGuardWidth = 100, swordLength = 1000 };
+
+            Console.WriteLine(orig);
+            Console.WriteLine(origWithGem);
+            Console.WriteLine(origWithAll);
+
+        }
+    }
+
+    record Sword(Material material, Gemstone gemstone, int swordLength = 0, int crossGuardWidth = 0);
+
+    enum Material { Wood, Bronze, Iron, Steel, Binarium };
+    enum Gemstone { Emerald, Amber, Sapphire, Diamond, Bitstone, None };
+}
